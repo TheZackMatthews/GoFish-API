c5a0187a88f84068648950d06cb8170a28a5758b
TheZackMatthews <zackary.matthews@yahoo.com>
2021-05-07 10:31:36 -0700

    Feat: Initial boilerplate
    listed a few basic endpoints, configured eslint, package.json

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const routes = require('./routes');
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+const host = process.env.HOST || 'localhost';
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(port, host, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on http://${host}:${port}`);
+});
